c30be1b7faf169379460cc3b74489d8fed9ac9bb
Geninnd <geninnd@outlook.com>
2025-06-12 20:52:14 -0300

    teste webhook

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ app = FastAPI(title="Infra Challenge App")
 
 @app.get("/")
 async def home():
-    return {"message": "Hello World - Desafio Infra. Teste para o sync do argoCD"}
+    return {"message": "Hello World - Desafio Infra. Teste para o webhook da pipeline"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
